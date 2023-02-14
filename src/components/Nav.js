f71e32c876ceae4325ce38e103a22ecb286a4a0d
Saba Ahmad <sabaahmad895@gmail.com>
2023-02-14 17:16:20 +0500

    some components

---
+++ src/components/Nav.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { IoChevronBack, IoMicSharp, IoSettingsOutline } from 'react-icons/io5';
+
+import PropTypes from 'prop-types';
+
+function Navbar(props) {
+  const { head, year } = props;
+  return (
+    <>
+      <header id="header">
+        <Link className="Link" to="/">
+          <IoChevronBack />
+          {year}
+        </Link>
+        {head}
+        <div className="navbar">
+          <IoMicSharp />
+          <IoSettingsOutline />
+        </div>
+      </header>
+    </>
+  );
+}
+Navbar.propTypes = {
+  head: PropTypes.string.isRequired,
+  year: PropTypes.number,
+};
+Navbar.defaultProps = {
+  year: null,
+};
+export default Navbar;
